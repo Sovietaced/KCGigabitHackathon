a83723bafe70d22ae4772c785a40fa26465605ff
Sovietaced <Sovietaced@gmail.com>
2013-03-23 14:01:55 -0500

    Got multiple threaded uploading to work, currently maxing at 4 threads. I will now test for more

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,39 @@
 import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
+	final static int THREADS = Runtime.getRuntime().availableProcessors();
+    static ExecutorService executor = Executors.newFixedThreadPool(THREADS);
+    
 	public static void main(String[] args) throws Exception {
 		System.out.println("Start");
-        FTPUploader ftpUploader = null;
-        try {
-			ftpUploader = new FTPUploader("sportsphotos.com", "jason", "jason");
-        } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        if (ftpUploader != null){
 	        File folder = new File("/home/jason/Desktop/photos/");
 	        File[] listOfFiles = folder.listFiles();
-	
 	            for (int i = 0; i < listOfFiles.length; i++) {
 	              if (listOfFiles[i].isFile()) {
 	                File file = listOfFiles[i];
-	    	        ftpUploader.uploadFile(file.getAbsolutePath(), file.getName(), "/");
-	                System.out.println("Done");
-              	}
+	                upload(file);
         	}
         }
-        ftpUploader.disconnect();
+	}
+	
+	public static void upload(final File file){
+		executor.submit(new Callable<String>(){
+        	public String call(){
+        		FTPUploader ftpUploader = null;
+        		try {
+        			ftpUploader = new FTPUploader("sportsphotos.com", "jason", "jason");
+					ftpUploader.uploadFile(file.getAbsolutePath(), file.getName(), "/" );
+					ftpUploader.disconnect();
+        		} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return "File Uploaded : " + file.getName();
+        	}
+        });
 	}
 }
